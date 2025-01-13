7709930a0a65ec52a1bcb85d7fc6562d316c2df7
Ghislain DUGAT <ghislain.dugat@univ-amu.fr>
2025-01-13 10:52:43 +0100

    chore: removing liquibase for now -changing server port

--- src/main/java/fr/avenirsesr/referentiels/AvenirsReferentielsApplication.java
+++ src/main/java/fr/avenirsesr/referentiels/AvenirsReferentielsApplication.java
@@ -2,8 +2,12 @@ package fr.avenirsesr.referentiels;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "fr.avenirsesr.referentiels")
+@EntityScan("fr.avenirsesr.referentiels.model")
 public class AvenirsReferentielsApplication {
 
 	public static void main(String[] args) {
